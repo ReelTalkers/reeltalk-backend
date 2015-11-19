dbab80d603e121bd602c77c13cf14c474f949c56
Greg Ziegan <greg.ziegan@gmail.com>
2015-11-19 14:19:45 -0500

    broke graphiql field typeahead but can query on one-to-one fields

--- reeltalk/schema.py
+++ reeltalk/schema.py
@@ -1,7 +1,13 @@
 import graphene
 from graphene import resolve_only_args, relay
 from graphene.contrib.django import DjangoNode, DjangoConnectionField
-from graphql.core.type import GraphQLArgument, GraphQLString
+from graphql.core.type import (
+    GraphQLArgument,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLFloat,
+    GraphQLBoolean
+)
 import re
 
 from . import models
@@ -13,7 +19,6 @@ class Connection(relay.Connection):
     total_count = graphene.IntField()
 
     def resolve_total_count(self, args, info):
-        print(dir(self))
         return len(self.get_connection_data())
 
 
@@ -109,32 +114,93 @@ class UserProfile(DjangoNode):
         exclude_fields = ('created', 'edited', 'curatedlist', 'group', 'review')
 
 
-def get_filterable_fields(model):
-    is_valid = lambda field: not field.is_relation and field.name not in ['created', 'edited', 'id']
-    return [field.name for field in model._meta.get_fields() if is_valid(field) ]
+def get_filterable_fields(model, accept_relations=True):
+    is_proper_relation = lambda field: accept_relations and field.one_to_one
+    is_valid = lambda field: is_proper_relation(field) if field.is_relation else True
+    is_excluded = lambda field: field.name in ['created', 'edited', 'id']
+    return [field for field in model._meta.get_fields() if is_valid(field) and not is_excluded(field) ]
+
+def get_graphql_type(field):
+    field_type = field.get_internal_type()
+    if 'Integer' in field_type:
+        return GraphQLInt
+    elif 'Decimal' in field_type:
+        return GraphQLFloat
+    elif 'Boolean' in field_type:
+        return GraphQLBoolean
+    else:
+        return GraphQLString
 
 def get_graphql_filter_arguments(fields):
-    return { to_camel_case(field): GraphQLArgument(GraphQLString) for field in fields }
+    filter_args = {}
+    for field in fields:
+        if field.is_relation:
+            for relation_field in get_filterable_fields(field.related_model, accept_relations=False):
+                key = '{}__{}'.format(to_camel_case(field.name), to_camel_case(relation_field.name))
+                filter_args[key] = GraphQLArgument(get_graphql_type(relation_field))
+        else:
+            filter_args[to_camel_case(field.name)] = GraphQLArgument(get_graphql_type(field))
+    return filter_args
 
-def extract_model_fields(model, all_fields):
-    return {to_snake_case(k):v for k, v in all_fields.items() if to_snake_case(k) in get_filterable_fields(model)}
+def extract_model_filters(model, all_fields):
+    model_filters = {}
+    for k, v in all_fields.items():
+        field_name = to_snake_case(k)
+        if '__' in k:
+            model_filters[field_name] = v
+            continue
+        if field_name in [field.name for field in get_filterable_fields(model)]:
+            model_filters[field_name] = v
+    return model_filters
 
 def to_camel_case(term):
-   first, *rest = term.split('_')
-   return first + ''.join(word.capitalize() for word in rest)
+    if '__' in term:
+        terms = term.split('__')
+        term1 = to_camel_case_simple(terms[0])
+        term2 = to_camel_case_simple(terms[1])
+        return '{}__{}'.format(term1, term2)
+    else:
+        return to_camel_case_simple(term)
+
+def to_camel_case_simple(term):
+    first, *rest = term.split('_')
+    return first + ''.join(word.capitalize() for word in rest)
 
 def to_snake_case(term):
+    if '__' in term:
+        terms = term.split('__')
+        term1 = to_snake_case_simple(terms[0])
+        term2 = to_snake_case_simple(terms[1])
+        return '{}__{}'.format(term1, term2)
+    else:
+        return to_snake_case_simple(term)
+
+def to_snake_case_simple(term):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', term)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
 class Query(graphene.ObjectType):
-    all_shows = DjangoConnectionField(Show)
-    all_reviews = DjangoConnectionField(Review)
-    all_users = DjangoConnectionField(User)
-    all_user_profiles = DjangoConnectionField(UserProfile)
-    all_groups = DjangoConnectionField(Group)
-    all_people = DjangoConnectionField(Person)
+    all_shows = DjangoConnectionField(
+        Show,
+        **get_graphql_filter_arguments(get_filterable_fields(models.Show))
+    )
+    all_reviews = DjangoConnectionField(
+        Review,
+        **get_graphql_filter_arguments(get_filterable_fields(models.Review))
+    )
+    all_user_profiles = DjangoConnectionField(
+        UserProfile,
+        **get_graphql_filter_arguments(get_filterable_fields(models.UserProfile))
+    )
+    all_groups = DjangoConnectionField(
+        Group,
+        **get_graphql_filter_arguments(get_filterable_fields(models.Group))
+    )
+    all_people = DjangoConnectionField(
+        Person,
+        **get_graphql_filter_arguments(get_filterable_fields(models.Person))
+    )
     all_curated_lists = DjangoConnectionField(CuratedList)
     person = relay.NodeField(Person)
     show = relay.NodeField(Show)
@@ -145,88 +211,36 @@ class Query(graphene.ObjectType):
     group = relay.NodeField(Group)
     node = relay.NodeField()
     viewer = graphene.Field('self')
-    filter_curated_lists = relay.ConnectionField(
-        CuratedList,
-        **get_graphql_filter_arguments(get_filterable_fields(models.CuratedList))
-    )
-    filter_groups = relay.ConnectionField(
-        Group,
-        **get_graphql_filter_arguments(get_filterable_fields(models.Group))
-    )
-    filter_people = relay.ConnectionField(
-        Person,
-        **get_graphql_filter_arguments(get_filterable_fields(models.Person))
-    )
-    filter_reviews = relay.ConnectionField(
-        Review,
-        **get_graphql_filter_arguments(get_filterable_fields(models.Review))
-    )
-    filter_shows = relay.ConnectionField(
-        Show,
-        **get_graphql_filter_arguments(get_filterable_fields(models.Show))
-    )
-    filter_user_profiles = relay.ConnectionField(
-        UserProfile,
-        **get_graphql_filter_arguments(get_filterable_fields(models.UserProfile))
-    )
-
-    @resolve_only_args
-    def resolve_filter_curated_lists(self, **kwargs):
-        model_filters = extract_model_fields(models.CuratedList, kwargs)
-        return models.CuratedList.objects.filter(**model_filters)
-
-    @resolve_only_args
-    def resolve_filter_groups(self, **kwargs):
-        model_filters = extract_model_fields(models.Group, kwargs)
-        return models.Group.objects.filter(**model_filters)
-
-    @resolve_only_args
-    def resolve_filter_people(self, **kwargs):
-        model_filters = extract_model_fields(models.Person, kwargs)
-        return models.Person.objects.filter(**model_filters)
-
-    @resolve_only_args
-    def resolve_filter_reviews(self, **kwargs):
-        model_filters = extract_model_fields(models.Review, kwargs)
-        return models.Review.objects.filter(**model_filters)
-
-    @resolve_only_args
-    def resolve_filter_shows(self, **kwargs):
-        model_filters = extract_model_fields(models.Show, kwargs)
-        return models.Show.objects.filter(**model_filters)
-
-    @resolve_only_args
-    def resolve_filter_user_profiles(self, **kwargs):
-        model_filters = extract_model_fields(models.UserProfile, kwargs)
-        return models.UserProfile.objects.filter(**model_filters)
-
-    @resolve_only_args
-    def resolve_all_shows(self, **kwargs):
-        return models.Show.objects.all()
-
-    @resolve_only_args
-    def resolve_all_reviews(self, **kwargs):
-        return models.Review.objects.all()
-
-    @resolve_only_args
-    def resolve_all_users(self, **kwargs):
-        return models.User.objects.all()
-
-    @resolve_only_args
-    def resolve_all_user_profiles(self, **kwargs):
-        return models.UserProfile.objects.all()
-
-    @resolve_only_args
-    def resolve_all_people(self, **kwargs):
-        return models.Person.objects.all()
-
-    @resolve_only_args
-    def resolve_all_groups(self, **kwargs):
-        return models.Group.objects.all()
 
     @resolve_only_args
     def resolve_all_curated_lists(self, **kwargs):
-        return models.CuratedList.objects.all()
+        model_filters = extract_model_filters(models.CuratedList, kwargs)
+        return models.CuratedList.objects.filter(**model_filters)
+
+    @resolve_only_args
+    def resolve_all_groups(self, **kwargs):
+        model_filters = extract_model_filters(models.Group, kwargs)
+        return models.Group.objects.filter(**model_filters)
+
+    @resolve_only_args
+    def resolve_all_people(self, **kwargs):
+        model_filters = extract_model_filters(models.Person, kwargs)
+        return models.Person.objects.filter(**model_filters)
+
+    @resolve_only_args
+    def resolve_all_reviews(self, **kwargs):
+        model_filters = extract_model_filters(models.Review, kwargs)
+        return models.Review.objects.filter(**model_filters)
+
+    @resolve_only_args
+    def resolve_all_shows(self, **kwargs):
+        model_filters = extract_model_filters(models.Show, kwargs)
+        return models.Show.objects.filter(**model_filters)
+
+    @resolve_only_args
+    def resolve_all_user_profiles(self, **kwargs):
+        model_filters = extract_model_filters(models.UserProfile, kwargs)
+        return models.UserProfile.objects.filter(**model_filters)
 
     def resolve_viewer(self, *args, **kwargs):
         return self
