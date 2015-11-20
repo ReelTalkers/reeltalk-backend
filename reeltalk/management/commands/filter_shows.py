d84ad2bb0df8e32bc56925781414bde4714ea9e5
Galen Caldwell <gcc17@case.edu>
2015-11-20 01:45:41 -0500

    Finished possibleMovieDict generation
    Signed-off-by: Galen Caldwell <gcc17@case.edu>

--- reeltalk/management/commands/filter_shows.py
+++ reeltalk/management/commands/filter_shows.py
@@ -19,7 +19,25 @@ class Command(BaseCommand):
                             matches+=1
                     if(matches>0):
                         similarityList.append((u.id, 2 - totalDifference / matches))
-            similarityList = [user[1] for user in similarityList if user[1] > 0]
+            similarityList = [user for user in similarityList if user[1] > 0]
             return similarityList
 
-        print(findSimilarUsers(userlist.first(), userlist))
+        def findMovieRecommendations( currentUser, similarUsers):
+            currentUserReviews = Review.objects.filter(user = currentUser)
+            possibleMovieDict = {}
+            for u in similarUsers:
+                reviewedMovies = Review.objects.filter(user__id = u[0])
+                for r in reviewedMovies:
+                    if(!currentUserReviews.filter(show__id=r.show.id).exists()):
+                        weightedScore = (r.score - 3)*u[1]
+                    if(possibleMovieMap.has_key(r.show.id)):
+                        totalWeightedScore = possibleMovieMap[r.show.id] + weightedScore
+                        possibleMovieMap[r.show.id] = totalWeightedScore
+                    else:
+                        possibleMovieMap[r.show.id] = weightedScore
+            return possibleMovieMap
+
+        similarUsers = findSimilarUsers(userlist.first(), userlist)
+        print(findMovieRecommendations(userlist.first(), similarUsers))
+
+        # print(findSimilarUsers(userlist.first(), userlist))
